974546c478caf102c178a944c0a6d4d2b43951c8
LeandroMelloo <leandromoreno1909@hotmail.com>
2018-12-23 19:43:24 -0200

    String.py

--- Numeros.py
+++ Numeros.py
@@ -9,3 +9,33 @@ b = 2
 
 #Printando uma variavel => b.
 print(b)
+
+# somando duas variaveis => a + b = 3.
+print(a + b)
+
+# subtraindo duas variaveis => a - b = -1.
+print(a - b)
+
+# multiplicando duas variaveis => a * b = 2.
+print(a * b)
+
+# dividindo duas variaveis => a / b = 0.5.
+print(a / b)
+
+# declarando uma variavel receita = 1000.
+receita = 1000
+
+# printando a variavel receita.
+print(receita)
+
+# declarando uma variavel margem_de_lucro = 0.10.
+margem_de_lucro = 10
+
+# printando a variavel margem_de_lucro.
+print(margem_de_lucro)
+
+# declarando uma variavel lucro = receita * margem_de_lucro.
+lucro = receita * margem_de_lucro
+
+# printando a variavel lucro.
+print(lucro)
--- String.py
+++ String.py
@@ -9,3 +9,57 @@ print('Olá Mundo, "tudo bem"?') # => valid syntax.
 
 #Citação utilizando aspas simples => '', da erro.
 # print('Olá Mundo, 'tudo bem'?') => invalid syntax.
+
+# printando o tamanho da String => len.
+print(len('Olá Mundo, "tudo bem"?')) # => valid syntax.
+
+# declarando uma variavel s.
+s = 'Olá Mundo'
+
+# printando a variavel s.
+print(s)
+
+# printando o tipo da variavel s.
+print(type(s))
+
+# printando a posicao 1 => l da variavel s, indexação do ponto.
+print(s[1])
+
+# ignorando as duas primeiras posições e as 3 últimas posições printando a palavra 'Mundo', indexação múltiplos.
+print(s[4:9])
+
+# começando do final pro inicio e ignorando as três últimas posições.
+print(s[:-3])
+
+#indexação de espaçamentos de elementos de 2 em 2.
+print(s[::2])
+
+#indexação de espaçamentos de elementos de de 3 em 3.
+print(s[::3])
+
+#invertendo strings pegando elementos de -1.
+print(s[::-1])
+
+#concatenando string => +.
+s = s + ', que dia lindo!!!'
+
+#printando a variavel s concatenada.
+print(s)
+
+#declarando a variavel letra.
+letra = 'g'
+
+#printando a variavel g.
+print(letra)
+
+#printando 10 x a variavel letra => 'gggggggggg'
+print(letra * 10)
+
+#objetos tem seu metodo especifico s.lower() => coloca tudo em caixa baixa, minusculo.
+print(s.lower())
+
+#objetos tem seu metodo especifico s.split() => dividi minha string.
+print(s.split())
+
+#objetos tem seu metodo especifico s.split() => dividi minha string em 2 objetos.
+print(s.split(','))
