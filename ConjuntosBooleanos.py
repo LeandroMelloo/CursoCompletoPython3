ebdab9d5a45ee85edd416efe14e04f45200f4ebe
LeandroMelloo <leandromoreno1909@hotmail.com>
2018-12-24 16:28:33 -0200

    ConjuntosBooleanos.py

--- ConjuntosBooleanos.py
+++ ConjuntosBooleanos.py
@@ -33,3 +33,23 @@ b = False
 
 # Printando elementos 
 print(1 > 2)
+
+#Prática básica:
+
+#http://codingbat.com/python
+
+#Mais prática matemática (e mais difícil):
+
+#https://projecteuler.net/archives
+
+#Lista de problemas práticos:
+
+#http://www.codeabbey.com/index/task_list
+
+#Um SubReddit dedicado a problemas de prática diária:
+
+#https://www.reddit.com/r/dailyprogrammer
+
+#Um site muito complicado com muito poucas dicas e problemas (Não para iniciantes, mas ainda interessantes)
+
+#http://www.pythonchallenge.com/
