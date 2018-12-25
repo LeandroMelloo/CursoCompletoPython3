aaff05777b090ab4c6b649b6d178f9ab2e6f36dd
LeandroMelloo <leandromoreno1909@hotmail.com>
2018-12-25 17:29:37 -0200

    OperacaoComparacao.py

---
+++ OperadoresComparacao.py
@@ -0,0 +1,23 @@
+# Operadores
+
+# Criando duas variaveis 'a' 'b' e comparando valores.
+a = 1
+b = 2
+
+# Printando a comparação entre as variaveis com o método igual => ==.
+print(a == b) # False.
+
+# Printando a comparação entre as variaveis com o método diferente => !=.
+print(a != b) # True.
+
+# Printando a comparação entre as variaveis com o método menor => <.
+print(a < b) # True.
+
+# Printando a comparação entre as variaveis com o método menor => >.
+print(a > b) # False.
+
+# Printando a comparação entre as variaveis com o método menor ou igual => <=.
+print(a <= b) # True.
+
+# Printando a comparação entre as variaveis com o método maior ou igual => >=.
+print(a >= b) # False.
